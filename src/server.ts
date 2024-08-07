ec0e445fabe451cb13849d98266f8811981db619
ZahirulIslamJoy <neloy1613@gmail.com>
2024-08-07 19:32:19 +0600

    handle server error

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import app from './app';
 import mongoose from 'mongoose';
 import config from './app/config';
+import { Server } from 'http';
+
+
+let server:Server;
 
 async function main() {
   try {
@@ -14,3 +18,21 @@ async function main() {
 }
 
 main();
+
+//handling asynchronous tasks 
+process.on("unhandledRejection",()=>{
+  console.log("unhandledRejection Found,server is shutting down")
+  if(server){
+    server.close(()=>{
+      process.exit(1)
+    })
+  }
+  else{
+    process.exit(1)
+  }
+})
+process.on("uncaughtException" , ()=>{
+  console.log("uncaughtException Found,server is shutting down")
+  process.exit(1)
+})
+
